90637cef50440cd3dcb8f739e5049994ee8d2994
ashutoshagarwal100 <ashutoshagarwal100@gmail.com>
2024-10-19 13:39:50 +0000

    Initialize Feasto - Food Ordering system

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header/Header"
+
+function App() {
+  return (
+    <div className="bg-gray-300/50 h-500 rounded-md">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import CartLogo from "../../../public/assets/cart.svg";
+
+const Header = () => {
+    return (
+        <div className="bg-white pt-6 px-4 h-20 flex justify-between items-center">
+            {/* Logo and Brand Name*/}
+            <div className="flex items-center">
+                <img src="../../../public/assets/salad.png" alt="logo" className="h-14" />
+                <span className="text-2xl ml-3 font-bold knewave-regular tracking-widest">
+                    Feasto
+                </span>
+            </div>
+            {/* Menu */}
+            <nav role="navigation" className="flex items-center text-xl">
+                <span className="px-4 m-2">Home</span>
+                <span className="px-4 m-2">About Us</span>
+                <span className="px-4 m-2">Help</span>
+                <span className="px-4 m-2">Home</span>
+            </nav>
+            {/* Cart */}
+            <div className="flex item-center px-4 p-2 cursor-pointer bg hover:bg-gray-300/25">
+                <img src={CartLogo} alt="cart-logo" />
+                <span className="m-2 text-xl">Cart</span>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
