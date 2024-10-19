f3b831675c0d8fb7a2719df8699453fdd8854ca4
ashutoshagarwal100 <ashutoshagarwal100@gmail.com>
2024-10-19 21:45:51 +0000

    Restaurants listed

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,6 +1,7 @@
 import CuisineComponent from "../CuisineComponent/CuisineComponent";
 import { useEffect, useState } from "react";
 import { RESTAURANT_DATA_URL } from "../../utils/common";
+import RestaurantCard from "../RestaurantCard/RestaurantCard";
 
 const Body = () => {
 
@@ -18,25 +19,16 @@ const Body = () => {
     }
 
     return (
-        <div id="body-container">
+        <div id="body-container" className="pb-4">
             {/* Cuisines */}
             <CuisineComponent resData={resData?.[0]} />
             {/* Restaurants list */}
             <div id="restaurant-card-container" className="mx-24">
                 <p className="p-4 mt-4 text-[1.6rem] font-bold">Top Restaurants</p>
-                <div className="grid gap-20 grid-cols-4 m-6">
-                    {/* {resData?.[1]?.card?.card?.gridElements?.infoWithStyles?.restaurants.map(restaurants => (
-
-                    ))} */}
-                    <div className="grid gap-3">
-                        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/17/9b88104a-587e-47e8-8b08-bc6d624f9b69_72605.jpg" alt="restaurant--image" className="h-64 w-full"/>
-                        <div>
-                        <p>Pizza Hut</p>
-                        <p><span>4.2</span> • <span>30-35 mins</span></p>
-                        <p>Pizza</p>
-                        <p>400 for two</p>
-                        </div>
-                    </div>
+                <div className="grid gap-12 grid-cols-4 m-6">
+                    {resData?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants.map(restaurant => (
+                        <RestaurantCard imageURL={restaurant?.info?.cloudinaryImageId} name={restaurant?.info?.name} rating={restaurant?.info?.avgRating} avgTime={restaurant?.info?.sla?.slaString} cuisines={restaurant?.info.cuisines} price={restaurant?.info?.costForTwo} />
+                    ))}
                 </div>
             </div>
         </div>
--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,6 +1,16 @@
-const RestaurantCard = () => {
-    return(
-        <></>
+const RestaurantCard = (props) => {
+    const { imageURL, name, rating, avgTime, cuisines, price } = props;
+
+    return (
+        <div className="grid gap-1 rounded-xl">
+            <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + imageURL} alt="restaurant--image" className="h-44 w-full rounded-xl object-cover" />
+            <div className="px-2">
+                <p className="font-bold text-xl my-1 text-ellipsis overflow-hidden ...">{name}</p>
+                <p><span className="font-semibold">{rating}</span> • <span className="font-semibold">{avgTime}</span></p>
+                <p className="text-gray-500 text-ellipsis overflow-hidden">{cuisines?.slice(0, 4).join(", ")}</p>
+                <p className="text-gray-500">{price}</p>
+            </div>
+        </div>
     );
 }
 
